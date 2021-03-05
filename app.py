a69f5954c4126710a1fb57e745ba7e18db158add
Beksultan Zhumirov <Beksultan.Zhumirov@erg.kz>
2021-03-05 11:16:09 +0600

    Added CORS

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 from flask_jwt import JWT
 from db import db
 from resources.store import Store, StoreList
+from flask_cors import CORS
 
 
 from security import authenticate, identity
@@ -12,6 +13,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
